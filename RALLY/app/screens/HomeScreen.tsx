741dce2f2ef5eb00c0e5c6c890052b2d86403a7e
Jake Zocco <jake.zocco@gmail.com>
2025-02-23 13:29:50 -0500

    Updated Homescreen with some basic ReactNative Function

--- RALLY/app/screens/HomeScreen.tsx
+++ RALLY/app/screens/HomeScreen.tsx
@@ -1,9 +1,32 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, Image, TextInput } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text style={{ fontSize: 20 }}> Home Screen</Text>
-    </View>
+    //Starts a scroll version of a view
+    <ScrollView>
+      <Text>Some text</Text>
+      <View 
+        style={{ 
+          flex: 1, 
+          justifyContent: "center", 
+          alignItems: "center" 
+        }}>
+        <Text style={{ fontSize: 20 }}> Home Screen</Text>
+        <Image //Example of how to make an image
+          source={{
+            uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
+          }}
+          style={{width: 200, height: 200}}
+        /> 
+      </View>
+      <TextInput //Text box
+        style={{
+          height: 40,
+          borderColor: 'gray',
+          borderWidth: 1,
+        }}
+        defaultValue="You can type in me" // Start value 
+      />
+    </ScrollView>
   );
 }
