1700b05af05497af7ff78972f75702087319b1d8
Jake Zocco <jake.zocco@gmail.com>
2025-02-24 15:02:14 -0500

    Added Basic Button to HomeScreen.

--- RALLY/app/screens/HomeScreen.tsx
+++ RALLY/app/screens/HomeScreen.tsx
@@ -1,17 +1,42 @@
-import { View, Text, ScrollView, Image, TextInput } from "react-native";
+import { View, Text, ScrollView, Image, TextInput, Button } from "react-native";
+import React, {useState} from 'react';
 
 export default function HomeScreen() {
+
+  type LocationInfo = {
+    name: string;
+  }
+
+  const Location = (info: LocationInfo) => {
+    const [attending, setAttending] = useState(false);
+    return (
+      <View>
+        <Text>
+          {info.name} : {attending ? 'I AM GOING!' : 'Not Going'}
+        </Text>
+        <Button
+          onPress={() => {
+            setAttending(!attending);
+          }}
+          title={attending ? 'Click to Unattend' : 'Click to Attend'}
+        />
+      </View>
+    );
+  };
+
   return (
     //Starts a scroll version of a view
     <ScrollView>
-      <Text>Some text</Text>
       <View 
         style={{ 
           flex: 1, 
           justifyContent: "center", 
           alignItems: "center" 
         }}>
-        <Text style={{ fontSize: 20 }}> Home Screen</Text>
+        <Text style={{ fontSize: 20 }}>Home Screen</Text>
+        <Location name = "Location 1"/>
+        <Location name = "Location 2"/>
+        <Text>Example image</Text>
         <Image //Example of how to make an image
           source={{
             uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
@@ -25,7 +50,7 @@ export default function HomeScreen() {
           borderColor: 'gray',
           borderWidth: 1,
         }}
-        defaultValue="You can type in me" // Start value 
+        defaultValue="Type Box" // Start value 
       />
     </ScrollView>
   );
